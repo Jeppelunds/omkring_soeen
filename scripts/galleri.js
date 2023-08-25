7847f55404a4f225ec96e493fe57b23a0c484679
Jeppelunds <mickijeppe@gmail.com>
2023-08-25 13:55:06 +0200

    modal lavede til galleri

---
+++ scripts/galleri.js
@@ -0,0 +1,220 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const Boat1 = document.querySelector(".boat1");
+  const Boat1Modal = document.querySelector(".boat1-modal");
+  const Close1 = Boat1Modal.querySelector(".fa-x"); 
+  
+  const Boat2 = document.querySelector(".boat2");
+  const Boat2Modal = document.querySelector(".boat2-modal");
+  const Close2 = Boat2Modal.querySelector(".fa-x"); 
+  
+  const Boat3 = document.querySelector(".boat3");
+  const Boat3Modal = document.querySelector(".boat3-modal");
+  const Close3 = Boat3Modal.querySelector(".fa-x");
+
+  const Boat4 = document.querySelector(".boat4");
+  const Boat4Modal = document.querySelector(".boat4-modal");
+  const Close4 = Boat4Modal.querySelector(".fa-x");
+
+  const Boat5 = document.querySelector(".boat5");
+  const Boat5Modal = document.querySelector(".boat5-modal");
+  const Close5 = Boat5Modal.querySelector(".fa-x");
+
+  const Boat6 = document.querySelector(".boat6");
+  const Boat6Modal = document.querySelector(".boat6-modal");
+  const Close6 = Boat6Modal.querySelector(".fa-x");
+
+  const Bridge1 = document.querySelector(".bridge1");
+  const Bridge1Modal = document.querySelector(".bridge1-modal");
+  const Close7 = Bridge1Modal.querySelector(".fa-x");
+
+  const Bridge2 = document.querySelector(".bridge2");
+  const Bridge2Modal = document.querySelector(".bridge2-modal");
+  const Close8 = Bridge2Modal.querySelector(".fa-x");
+
+  const Bridge3 = document.querySelector(".bridge3");
+  const Bridge3Modal = document.querySelector(".bridge3-modal");
+  const Close9 = Bridge3Modal.querySelector(".fa-x");
+
+  const Church1 = document.querySelector(".church1");
+  const Church1Modal = document.querySelector(".church1-modal");
+  const Close10 = Church1Modal.querySelector(".fa-x");
+
+  const Church2 = document.querySelector(".church2");
+  const Church2Modal = document.querySelector(".church2-modal");
+  const Close11 = Church2Modal.querySelector(".fa-x");
+
+  const Ducks1 = document.querySelector(".ducks1");
+  const Ducks1Modal = document.querySelector(".ducks1-modal");
+  const Close12 = Ducks1Modal.querySelector(".fa-x");
+
+  const Ducks2 = document.querySelector(".ducks2");
+  const Ducks2Modal = document.querySelector(".ducks2-modal");
+  const Close13 = Ducks2Modal.querySelector(".fa-x");
+
+  const Ducks3 = document.querySelector(".ducks3");
+  const Ducks3Modal = document.querySelector(".ducks3-modal");
+  const Close14 = Ducks3Modal.querySelector(".fa-x");
+
+  const Ducks4 = document.querySelector(".ducks4");
+  const Ducks4Modal = document.querySelector(".ducks4-modal");
+  const Close15 = Ducks4Modal.querySelector(".fa-x");
+
+  const Ducks5 = document.querySelector(".ducks5");
+  const Ducks5Modal = document.querySelector(".ducks5-modal");
+  const Close16 = Ducks5Modal.querySelector(".fa-x");
+
+  const Sivene1 = document.querySelector(".sivene1");
+  const Sivene1Modal = document.querySelector(".sivene1-modal");
+  const Close17 = Sivene1Modal.querySelector(".fa-x");
+
+  const Sivene2 = document.querySelector(".sivene2");
+  const Sivene2Modal = document.querySelector(".sivene2-modal");
+  const Close18 = Sivene2Modal.querySelector(".fa-x");
+
+  Boat1.addEventListener("click", () => {
+    Boat1Modal.classList.add("show-modal");
+  });
+
+  Boat2.addEventListener("click", () => {
+    Boat2Modal.classList.add("show-modal");
+  });
+
+  Boat3.addEventListener("click", () => {
+    Boat3Modal.classList.add("show-modal");
+  });
+
+  Boat4.addEventListener("click", () => {
+    Boat4Modal.classList.add("show-modal");
+  });
+
+  Boat5.addEventListener("click", () => {
+    Boat5Modal.classList.add("show-modal");
+  });
+
+  Boat6.addEventListener("click", () => {
+    Boat6Modal.classList.add("show-modal");
+  });
+
+  Bridge1.addEventListener("click", () => {
+    Bridge1Modal.classList.add("show-modal");
+  });
+
+  Bridge2.addEventListener("click", () => {
+    Bridge2Modal.classList.add("show-modal");
+  });
+
+  Bridge3.addEventListener("click", () => {
+    Bridge3Modal.classList.add("show-modal");
+  });
+
+  Church1.addEventListener("click", () => {
+    Church1Modal.classList.add("show-modal");
+  });
+
+  Church2.addEventListener("click", () => {
+    Church2Modal.classList.add("show-modal");
+  });
+
+  Ducks1.addEventListener("click", () => {
+    Ducks1Modal.classList.add("show-modal");
+  });
+
+  Ducks2.addEventListener("click", () => {
+    Ducks2Modal.classList.add("show-modal");
+  });
+
+  Ducks3.addEventListener("click", () => {
+    Ducks3Modal.classList.add("show-modal");
+  });
+
+  Ducks4.addEventListener("click", () => {
+    Ducks4Modal.classList.add("show-modal");
+  });
+
+  Ducks5.addEventListener("click", () => {
+    Ducks5Modal.classList.add("show-modal");
+  });
+
+  Sivene1.addEventListener("click", () => {
+    Sivene1Modal.classList.add("show-modal");
+  });
+
+  Sivene2.addEventListener("click", () => {
+    Sivene2Modal.classList.add("show-modal");
+  });
+
+  Close1.addEventListener("click", () => {
+    Boat1Modal.classList.remove("show-modal");
+  });
+
+  Close2.addEventListener("click", () => {
+    Boat2Modal.classList.remove("show-modal");
+  });
+
+  Close3.addEventListener("click", () => {
+    Boat3Modal.classList.remove("show-modal");
+  });
+
+  Close4.addEventListener("click", () => {
+    Boat4Modal.classList.remove("show-modal");
+  });
+
+  Close5.addEventListener("click", () => {
+    Boat5Modal.classList.remove("show-modal");
+  });
+
+  Close6.addEventListener("click", () => {
+    Boat6Modal.classList.remove("show-modal");
+  });
+
+  Close7.addEventListener("click", () => {
+    Bridge1Modal.classList.remove("show-modal");
+  });
+
+  Close8.addEventListener("click", () => {
+    Bridge2Modal.classList.remove("show-modal");
+  });
+
+  Close9.addEventListener("click", () => {
+    Bridge3Modal.classList.remove("show-modal");
+  });
+
+  Close10.addEventListener("click", () => {
+    Church1Modal.classList.remove("show-modal");
+  });
+
+  Close11.addEventListener("click", () => {
+    Church2Modal.classList.remove("show-modal");
+  });
+
+  Close12.addEventListener("click", () => {
+    Ducks1Modal.classList.remove("show-modal");
+  });
+
+  Close13.addEventListener("click", () => {
+    Ducks2Modal.classList.remove("show-modal");
+  });
+
+  Close14.addEventListener("click", () => {
+    Ducks3Modal.classList.remove("show-modal");
+  });
+
+  Close15.addEventListener("click", () => {
+    Ducks4Modal.classList.remove("show-modal");
+  });
+
+  Close16.addEventListener("click", () => {
+    Ducks5Modal.classList.remove("show-modal");
+  });
+
+  Close17.addEventListener("click", () => {
+    Sivene1Modal.classList.remove("show-modal");
+  });
+
+  Close18.addEventListener("click", () => {
+    Sivene2Modal.classList.remove("show-modal");
+  });
+
+});
+
+
