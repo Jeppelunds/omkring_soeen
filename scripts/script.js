cb6912cabe359edc20855a75a4a421f5eb36cf49
Jeppelunds <mickijeppe@gmail.com>
2023-08-28 13:40:39 +0200

    burger menu lavet, igang med desktop menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,13 @@
-
 console.log('hello world');
+
+const BurgerIcon = document.querySelector(".fa-bars")
+const BurgerMenu = document.querySelector(".burger-menu")
+
+BurgerIcon.addEventListener("click", () => {
+  BurgerMenu.classList.toggle("active");
+});
+
+BurgerMenu.addEventListener("click", () => {
+  BurgerMenu.classList.remove("active");
+});
+
