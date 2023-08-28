5a75d4ecb56ee0c5b40945cf040582ef8cc221a5
Jeppelunds <mickijeppe@gmail.com>
2023-08-28 22:15:24 +0200

    lavede lidt på menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 console.log('hello world');
 
+/* Burger menu */ 
+
 const BurgerIcon = document.querySelector(".fa-bars")
 const BurgerMenu = document.querySelector(".burger-menu")
 
@@ -10,4 +12,24 @@ BurgerIcon.addEventListener("click", () => {
 BurgerMenu.addEventListener("click", () => {
   BurgerMenu.classList.remove("active");
 });
+
+/* nav menu */ 
+
+const SectionRight = document.querySelector(".test");
+const FirstSectionMenu = document.querySelector("._2Nx_1");
+
+const observer = new IntersectionObserver(entries => {
+  const visibilityRatio = entries[0].intersectionRatio;
+  console.log(`Visibility Ratio: ${visibilityRatio}`);
+  
+  if (visibilityRatio >= 0.75) {
+    FirstSectionMenu.classList.add("show");
+  } else {
+    FirstSectionMenu.classList.remove("show");
+  }
+}, {
+  threshold: 0.75,
+});
+
+observer.observe(SectionRight);
 
