81abece77f6c9153198609087083aa3c3f67c0f8
Jeppelunds <mickijeppe@gmail.com>
2023-08-29 12:29:41 +0200

    navigation lavet

--- scripts/script.js
+++ scripts/script.js
@@ -15,21 +15,52 @@ BurgerMenu.addEventListener("click", () => {
 
 /* nav menu */ 
 
-const SectionRight = document.querySelector(".test");
-const FirstSectionMenu = document.querySelector("._2Nx_1");
+const SectionBoat = document.querySelector(".boat-menu");
+const BoatSectionMenu = document.querySelector("._2Nx_1");
+const SectionBridge = document.querySelector(".bridge-menu")
+const BridgeSectionMenu = document.querySelector("._2Nx_2");
+const SectionSivene = document.querySelector(".sivene-menu");
+const SiveneSectionMenu = document.querySelector("._2Nx_3");
+const SectionChurch = document.querySelector(".church-menu");
+const ChurchSectionMenu = document.querySelector("._2Nx_4");
+const SectionDucks = document.querySelector(".ducks-menu");
+const DucksSectionMenu = document.querySelector("._2Nx_5");
 
-const observer = new IntersectionObserver(entries => {
-  const visibilityRatio = entries[0].intersectionRatio;
-  console.log(`Visibility Ratio: ${visibilityRatio}`);
-  
-  if (visibilityRatio >= 0.75) {
-    FirstSectionMenu.classList.add("show");
+const sections = [
+  { section: SectionBoat, menu: BoatSectionMenu },
+  { section: SectionBridge, menu: BridgeSectionMenu },
+  { section: SectionSivene, menu: SiveneSectionMenu },
+  { section: SectionChurch, menu: ChurchSectionMenu },
+  { section: SectionDucks, menu: DucksSectionMenu }
+];
+
+const observerCallback = (menu, visibilityRatio) => {
+  if (visibilityRatio >= 0.8) {
+    menu.classList.add("show");
   } else {
-    FirstSectionMenu.classList.remove("show");
+    menu.classList.remove("show");
   }
-}, {
-  threshold: 0.75,
+};
+
+const observers = [];
+
+sections.forEach(({ section, menu }) => {
+  const observer = new IntersectionObserver(entries => {
+    const visibilityRatio = entries[0].intersectionRatio;
+    console.log(`Visibility Ratio: ${visibilityRatio}`);
+    observerCallback(menu, visibilityRatio);
+  }, {
+    threshold: 0.8,
+  });
+  
+  observer.observe(section);
+  observers.push(observer);
 });
 
-observer.observe(SectionRight);
+
+
+
+
+
+
 
